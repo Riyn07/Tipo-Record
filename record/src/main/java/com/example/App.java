b34a12fd0bff74d12fb7fa4cdb3136c4d2236d5a
Riyn07 <riynexodar@gmail.com>
2026-04-20 13:27:39 +0200

    Clase Abstracta explicada

--- record/src/main/java/com/example/App.java
+++ record/src/main/java/com/example/App.java
@@ -1,10 +1,12 @@
 package com.example;
 
-/**
- * Hello world!
- */
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    
+            // una clase abstracta no se puede instanciar
+            // es decir, no se pueden crear objetos de una clase abstracta
+            // la clase tiene que ser heredada por otra clase para poder crear objetos de esa clase hija
+            Persona persona = new Persona();
     }
 }
